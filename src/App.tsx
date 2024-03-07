a177d1b6b74b5952e2b04ef1a73e89757035a85e
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-03-07 14:59:14 +0900

    feat: implement create new raffle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { createRaffle } from "./features/raffle/raffleSlice";
+import { useAppDispatch } from "./app/hooks";
+
+interface RaffleForm {
+  name: string;
+}
 
 function App() {
-  return <div>hi</div>;
+  const { register, handleSubmit, reset } = useForm<RaffleForm>();
+  const dispatch = useAppDispatch();
+
+  const onSubmit = ({ name }: RaffleForm) => {
+    dispatch(createRaffle(name));
+    reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="이름을 입력하세요"
+          {...register("name", { required: true })}
+        />
+        <button>+</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
