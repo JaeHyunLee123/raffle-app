500daac9964e7cc910b323b19e00e91bc821e17f
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-03-07 20:12:16 +0900

    feat: implement ticket plus and minus button

--- src/features/raffle/Raffle.tsx
+++ src/features/raffle/Raffle.tsx
@@ -1,15 +1,28 @@
 import { FC } from "react";
-import type { RaffleObj } from "./raffleSlice";
+import { RaffleObj, updateByAmount } from "./raffleSlice";
+import { useAppDispatch } from "src/app/hooks";
 
 interface RaffleProps {
   raffle: RaffleObj;
 }
 
 const Raffle: FC<RaffleProps> = ({ raffle }) => {
+  const dispatch = useAppDispatch();
+
+  const onMinusClick = () => {
+    dispatch(updateByAmount({ amount: -1, id: raffle.id }));
+  };
+
+  const onPlusClick = () => {
+    dispatch(updateByAmount({ amount: 1, id: raffle.id }));
+  };
+
   return (
     <div>
       <span>name: {raffle.name}</span>
+      <button onClick={onMinusClick}>-</button>
       <span>tickets: {raffle.ticket}</span>
+      <button onClick={onPlusClick}>+</button>
     </div>
   );
 };
