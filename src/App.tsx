77fbe4e9a53f0f3683ac8ed2c5267505b2df36c9
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-03-14 15:22:44 +0900

    feat: add sytles to form

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,35 @@ import {
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import Raffle from "./features/raffle/Raffle";
 import Roulette from "./components/Roulette";
+import styled from "styled-components";
+
+const Form = styled.form`
+  margin: 1rem;
+  padding: 0.5rem;
+
+  input {
+    border: none;
+    border-bottom: 1px solid black;
+    font-size: 1.5rem;
+    margin-right: 0.5rem;
+  }
+
+  button {
+    margin-right: 2px;
+    margin-left: 2px;
+
+    padding-top: 0.3rem;
+    padding-bottom: 0.3rem;
+    padding-left: 0.7rem;
+    padding-right: 0.7rem;
+
+    border-radius: 0.5rem;
+    font-size: 1.2rem;
+
+    border: none;
+    background-color: #daf8e3;
+  }
+`;
 
 interface RaffleForm {
   name: string;
@@ -31,13 +60,13 @@ function App() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <input
           placeholder="이름을 입력하세요"
           {...register("name", { required: true })}
         />
         <button>추가</button>
-      </form>
+      </Form>
       <div>
         <span>Total tickets: {totalTickets}</span>
       </div>
