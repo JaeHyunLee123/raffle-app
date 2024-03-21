4728c089d57d5f756468e6c4f876c8c095af4222
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2024-03-21 15:05:33 +0900

    feat: rotating

--- src/components/Roulette.tsx
+++ src/components/Roulette.tsx
@@ -4,8 +4,8 @@ import {
   selectRaffle,
   selectTotalTickets,
 } from "src/features/raffle/raffleSlice";
-import { FC, useRef, useState } from "react";
-import styled from "styled-components";
+import { FC, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import { motion } from "framer-motion";
 
 const RouletteBtn = styled.button`
@@ -29,19 +29,25 @@ const WinnerSpan = styled.span`
   font-size: 1.5rem;
 `;
 
-const Circle = styled(motion.div)`
-  width: 30px;
-  height: 30px;
-  border: 3px solid black;
-  border-radius: 100%;
-  background-color: blue;
+const rotate = keyframes`
+    from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
-const RouletteBound = styled(motion.div)`
+const Circle = styled(motion.div)<{ $isRotate?: boolean }>`
   width: 500px;
   height: 500px;
   border: 3px solid black;
-  border-radius: 100px;
+  border-radius: 100%;
+  background-color: whitesmoke;
+  animation: ${rotate} 4s linear
+    ${(props) => (props.$isRotate ? "inifinte" : "")};
+  text-align: center;
 `;
 
 interface RouletteProps {}
@@ -50,6 +56,8 @@ const Roulette: FC<RouletteProps> = () => {
   const raffleList = useAppSelector(selectRaffle);
   const totalTickets = useAppSelector(selectTotalTickets);
   const dispatch = useAppDispatch();
+
+  const [isRotating, setIsRotating] = useState(false);
 
   const [winner, setWinner] = useState("");
 
@@ -76,17 +84,18 @@ const Roulette: FC<RouletteProps> = () => {
     }
   };
 
-  const constraintsRef = useRef(null);
-
   return (
     <div>
       <RouletteBtn onClick={onRouletteClick}>룰렛 실행</RouletteBtn>
       {winner ? <WinnerSpan>당첨자: {winner}</WinnerSpan> : ""}
-      <RouletteBound ref={constraintsRef}>
-        {[...Array(10)].map((_, i) => (
-          <Circle key={i} />
-        ))}
-      </RouletteBound>
+      <button
+        onClick={() => {
+          setIsRotating(!isRotating);
+        }}
+      >
+        돌려라 돌림판
+      </button>
+      <Circle $isRotate={isRotating}>hello</Circle>
     </div>
   );
 };
